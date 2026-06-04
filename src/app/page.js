efa1499185ead0bd22589bef2a829cac17327ec3
TUPSC <studentcounciltriampat@gmail.com>
2026-06-04 13:11:01 +0700

    SIN
    SIN

--- src/app/page.js
+++ src/app/page.js
@@ -18,49 +18,61 @@ export default async function Home() {
         fontFamily: "Arial, sans-serif",
       }}
     >
-      <div
-        style={{
-          width: "100%",
-          background: "linear-gradient(90deg,#020817,#071633,#020817)",
-          padding: "14px 28px",
-          boxSizing: "border-box",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          position: "sticky",
-          top: 0,
-          zIndex: 9999,
-          boxShadow: "0 10px 30px rgba(0,0,0,0.18)",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "16px",
-          }}
-        >
-          <img
-            src="/logo.png"
-            alt="logo"
-            style={{
-              width: "76px",
-            }}
-          />
+    <div
+  style={{
+    width: "100%",
+    background: "linear-gradient(90deg,#020817,#071633,#020817)",
+    padding: "14px 28px",
+    boxSizing: "border-box",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    position: "sticky",
+    top: 0,
+    zIndex: 9999,
+    boxShadow: "0 10px 30px rgba(0,0,0,0.18)",
+  }}
+>
+  <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      gap: "18px",
+    }}
+  >
+    <img
+      src="/logo.png"
+      alt="logo"
+      style={{
+        width: "76px",
+      }}
+    />
 
-          <p
-            style={{
-              margin: 0,
-              color: "#8ea4ff",
-              fontSize: "15px",
-              fontWeight: "800",
-              letterSpacing: "1.5px",
-            }}
-          >
-            LIVE STUDENT COUNCIL VOTE COUNT
-          </p>
-        </div>
-      </div>
+    <div
+      style={{
+        color: "#8ea4ff",
+        fontSize: "15px",
+        fontWeight: "800",
+        letterSpacing: "1.5px",
+      }}
+    >
+      LIVE STUDENT COUNCIL VOTE COUNT
+    </div>
+  </div>
+
+  <div
+    style={{
+      color: "#8ea4ff",
+      fontSize: "15px",
+      fontWeight: "800",
+      letterSpacing: "1.5px",
+      textAlign: "right",
+      whiteSpace: "nowrap",
+    }}
+  >
+    *ผลคะแนนอย่างไม่เป็นทางการ*
+  </div>
+</div>
 
       <div
         style={{
